1adcda9b70b67c832dbdb66e09b7f1ba5ea3f1e8
Sean Roades <seanroades@gmail.com>
2020-11-18 12:37:52 -0800

    New updated blank page VarMail

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
-import office_pic from './office.jpg';
 import './App.css';
-import Entrybox from './Pages/Components/Entrybox/Entrybox';
 import Landing_page from './Pages/Landing_page/Landing_page'
 
-class App {
+class App extends Component{
   render() {
     return (
       <Landing_page />
--- src/Pages/Landing_page/Landing_page.js
+++ src/Pages/Landing_page/Landing_page.js
@@ -1,38 +1,8 @@
 import React from 'react';
-import './landing_page.css';
+
 const landing_page = () => {
-  class Landing_page extends Component{
-    state = {
-      entrybox: [
-        {placeholder: 'Name'}
-      ],
-    }
-    return (
-      <div className="Landing_page">
-        <header className="Landing-page-header">
-          <p>VarMail</p>
-        </header>
-
-        <body>
-
-          <p>It's hard to send personlized emails to your whole team. We think we can help.</p>
-            <div className="Landing-page-intro">
-              <div>
-                <img src={office_pic} alt="Office workers"></img>
-              </div>
-            </div>
-
-          <div>
-            <p>The internet's first drag and drop email service.</p>
-            <Entrybox placeholder="Name" />
-            <Entrybox placeholder="Email" />
-            <Entrybox placeholder="Password" />
-            <Entrybox placeholder="Confirm Password" />
-          </div>
-
-        </body>
-      </div>
-    )
-  }}};
-
+  return (
+    <header>VarMail</header>
+  )
+};
 export default landing_page;
