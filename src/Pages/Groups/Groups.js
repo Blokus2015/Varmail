fc99b738df4cb49182b3e521cd41962cd184b902
blokus2015 <seanroades@gmail.com>
2020-12-08 00:35:33 -0500

    created basic layout for create_survey page

--- src/Pages/Groups/Groups.js
+++ src/Pages/Groups/Groups.js
@@ -3,28 +3,52 @@ import './Groups.css';
 import Var_navbar from '../Components/Var_navbar/Var_navbar.js';
 import Button from 'react-bootstrap/Button';
 
+import { connect } from 'react-redux';
+import { firebaseConnect, isLoaded } from  'react-redux-firebase';
+import { compose } from 'redux';
+
 class Groups extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
   render(){
-      return (
-        <>
-        <head>
-          <link rel="preconnect" href="https://fonts.gstatic.com"/>
-          <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap" rel="stylesheet"/>
-        </head>
-          <Button to="/" variant="dark">Back</Button>
-          <div className="Groups_title">
-            <h1>Team members</h1>
-            <p>names go here</p>
-          </div>
-          <div class="ripple-background">
-            <div class="circle xxlarge shade1"></div>
-            <div class="circle xlarge shade2"></div>
-            <div class="circle large shade3"></div>
-            <div class="circle mediun shade4"></div>
-            <div class="circle small shade5"></div>
-          </div>
-        </>
-      )
+    if(!isLoaded(this.props.teamMembers)) {
+      return <div>Loading team teamMembers</div>
+    }
+    return (
+      <>
+      <head>
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap" rel="stylesheet"/>
+      </head>
+        <Button to="/" variant="dark">Back</Button>
+        <div className="Groups_title">
+          <h1>Team members</h1>
+          <p>{this.props.teamMembers.surveyQuestion1.answers.answer1}</p>
+          <p>{this.props.teamMembers.surveyQuestion1.answers.answer2}</p>
+
+        </div>
+        <div class="ripple-background">
+          <div class="circle xxlarge shade1"></div>
+          <div class="circle xlarge shade2"></div>
+          <div class="circle large shade3"></div>
+          <div class="circle mediun shade4"></div>
+          <div class="circle small shade5"></div>
+        </div>
+      </>
+    )
 }};
 
-export default Groups;
+const mapStateToProps = state => {
+  console.log(state);
+  const teamMembers = state.firebase.data.survey;
+  return { teamMembers: teamMembers };
+};
+
+export default compose(
+  firebaseConnect([{path:'/users/sean/Organization/Survey', storeAs: 'survey'}]),
+  connect(mapStateToProps),
+)(Groups);
--- src/Pages/Routes/Routes.js
+++ src/Pages/Routes/Routes.js
@@ -4,8 +4,8 @@ import { Switch, Route, BrowserRouter} from 'react-router-dom';
 import Landing_page from '../Landing_page/Landing_page.js';
 import Login from '../Login/Login.js';
 import Register_org from '../Register_org/Register_org.js';
-import Re_survey from '../Receive_survey/Receive_survey.js';
-import Cr_survey from '../Create_survey/Create_survey.js';
+import Receive_survey from '../Receive_survey/Receive_survey.js';
+import Create_survey from '../Create_survey/Create_survey.js';
 import Groups from '../Groups/Groups.js';
 import Homepage from '../Homepage/Homepage.js';
 import Error_page from '../Error_page/Error_page.js';
@@ -20,8 +20,8 @@ const Routes = () => {
           <Route exact path="/" component={Landing_page} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/register_org" component={Register_org} />
-          <Route exact path="/re_survey" component={Re_survey} />
-          <Route exact path="/cr_survey" component={Cr_survey} />
+          <Route exact path="/receive_survey" component={Receive_survey} />
+          <Route exact path="/create_survey" component={Create_survey} />
           <Route exact path="/groups" component={Groups} />
           <Route path="/homepage" component={Homepage} />
           <Route path="/testing" component={Test} />
