a181bc5301fb42289343ca8aaf43f388cf154758
blokus2015 <seanroades@gmail.com>
2020-12-07 20:36:07 -0500

    Connected Varmail to the firebase database. Ignore the test.js file, only using for firebase connection testing purposes temp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import Landing_page from './Pages/Landing_page/Landing_page';
 import { BrowserRouter } from 'react-router-dom';
 
 import Routes from './Pages/Routes/Routes.js'
